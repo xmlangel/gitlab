9e2ffbe2ef37ee109db6bedbc3f60e2d9dd79b56
kwangmyung Kim <kmkim@whatap.io>
2017-09-10 14:23:03 +0900

    정상

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8888:80 -p 22:22 -v$(pwd)/backups:/var/opt/gitlab/backups  --name gitlab  gitlab/gitlab-ce
+docker run -p 8888:80 -p 22:22 -v$(pwd)/backups:/home  --name gitlab  gitlab/gitlab-ce
